64750f02db78476af9b750e6c6e80d19e424a6ba
Naman Gaur <72210384+namangaur2@users.noreply.github.com>
2024-01-08 13:32:48 +0000

    basics updated

---
+++ basics/destrct.js
@@ -0,0 +1,9 @@
+const course = {
+    name: "naman",
+    price: 999,
+    instrcutor: "gaur"
+
+}
+
+const {instrcutor: inst, name: n, price: p} = course
+console.log({inst,n,p});
---
+++ basics/strings.js
@@ -0,0 +1,5 @@
+let name = "Naman";
+console.log(`my name is ${name}`)
+const max = 6;
+const min = 1;
+console.log(Math.floor(Math.random() * (max -min + 1)) + min)
